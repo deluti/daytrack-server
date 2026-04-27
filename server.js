b2d3bcb529a40c1091159fdfbcb54be0053f8881
deluti <delution27@gmail.com>
2026-04-27 21:18:25 +0700

    fix: исправлен CORS и синтаксическая ошибка

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Инициализация базы данных
